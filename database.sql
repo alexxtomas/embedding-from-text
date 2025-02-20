d488e43544772644fa62abbed700d1c0a525581d
Alex <atomasllimos@gmail.com>
2025-02-20 16:59:21 +0100

    innit

---
+++ database.sql
@@ -0,0 +1,28 @@
+-- Create a table to store your documents
+create table movies (
+  id bigserial primary key,
+  content text, 
+  embedding vector(1536) -- 1536 works for OpenAI embeddings
+);
+
+create or replace function match_movies (
+  query_embedding vector(1536),
+  match_threshold float,
+  match_count int
+)
+returns table (
+  id bigint,
+  content text, 
+  similarity float
+)
+language sql stable
+as $$
+  select
+    movies.id,
+    movies.content,
+    1 - (movies.embedding <=> query_embedding) as similarity
+  from movies
+  where 1 - (movies.embedding <=> query_embedding) > match_threshold
+  order by similarity desc
+  limit match_count;
+$$;
